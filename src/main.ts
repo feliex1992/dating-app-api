b7769450e17e308e1537c5cd5a29e03666085c33
Jafar Pahrudin <jafar@Jafars-MacBook-Air.local>
2024-08-16 01:29:21 +0700

    feat: config env.

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableVersioning({
-    type: VersioningType.URI
+    type: VersioningType.URI,
   });
-  
-  await app.listen(3000);
+
+  await app.listen(process.env.PORT || 3048);
 }
 bootstrap();
